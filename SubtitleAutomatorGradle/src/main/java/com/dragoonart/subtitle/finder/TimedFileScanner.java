ca2ee0ca69dd6b46eda007ffe28bd045b5bdb96b
DragooNArT-PC <DragooNArT-PC@DESKTOP-JICLDI6>
2017-10-02 01:33:24 +0300

    fix some parsing corner cases + writing files with wrong names bug

--- SubtitleAutomatorGradle/src/main/java/com/dragoonart/subtitle/finder/TimedFileScanner.java
+++ SubtitleAutomatorGradle/src/main/java/com/dragoonart/subtitle/finder/TimedFileScanner.java
@@ -27,6 +27,7 @@ public class TimedFileScanner extends SimpleFileVisitor<Path> implements Runnabl
 	private String[] movieExtensions = new String[] { "avi", "mpeg", "mkv", "mp4", "mpg", "" };
 
 	private List<VideoEntry> acceptedFiles = new ArrayList<>();
+
 	public TimedFileScanner(String path) {
 		rootFolder = Paths.get(path);
 	}
@@ -37,7 +38,7 @@ public class TimedFileScanner extends SimpleFileVisitor<Path> implements Runnabl
 			Files.walkFileTree(rootFolder, this);
 			logResults();
 			insertExactSubMatches();
-	
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -45,19 +46,22 @@ public class TimedFileScanner extends SimpleFileVisitor<Path> implements Runnabl
 	}
 
 	private void insertExactSubMatches() {
+		List<VideoEntry> toRemove = new ArrayList<VideoEntry>();
 		acceptedFiles.parallelStream().forEach(ve -> {
 			List<SubtitleArchiveEntry> sre = sl.getSubtitleZips(ve);
 			sre.parallelStream().forEach(e -> {
 				for (Entry<String, Path> entry : e.getSubtitleEntries().entrySet()) {
-					System.out.println("Name: " + entry.getKey() + "\nLocation: "
-							+ entry.getValue().toAbsolutePath().toString());
-					if(subtitleExactMatch(ve, entry)) {
-						//TODO remove subtitle from list, subtitles are found
+					System.out.println(
+							"Name: " + entry.getKey() + "\nLocation: " + entry.getValue().toAbsolutePath().toString());
+					if (subtitleExactMatch(ve, entry)) {
+						toRemove.add(ve);
 						break;
 					}
 				}
 			});
 		});
+		// remove files for which an exact match has been found
+		acceptedFiles.removeAll(toRemove);
 	}
 
 	private void logResults() {
@@ -79,33 +83,37 @@ public class TimedFileScanner extends SimpleFileVisitor<Path> implements Runnabl
 
 	private boolean subtitleExactMatch(VideoEntry ve, Entry<String, Path> entry) {
 		String release = ve.getParsedFilename().getRelease();
-		if(release != null) {
-				ParsedFileName pfn = null;
+		if (release != null) {
+			ParsedFileName pfn = null;
+			try {
+				pfn = new ParsedFileName(
+						entry.getKey().indexOf(".") > -1 ? entry.getKey().substring(0, entry.getKey().lastIndexOf("."))
+								: entry.getKey());
+			} catch (Throwable t) {
+				System.out.println("Bad entry: " + entry.getKey());
+				throw t;
+			}
+			String subRelease = pfn.getRelease();
+			if (release.equalsIgnoreCase(subRelease)) {
 				try {
-				pfn = new ParsedFileName(entry.getKey().substring(0, entry.getKey().lastIndexOf(".")));
-				} catch(Throwable t) {
-					System.out.println("Bad entry: "+entry.getKey());
-					throw t;
-				}
-				String subRelease = pfn.getRelease();
-				if(release.equalsIgnoreCase(subRelease)) {
-					try {
-						Path newFilePath = ve.getPathToFile().getParent().resolve(ve.getFileName() + entry.getKey().substring(entry.getKey().lastIndexOf(".")));
-						if(!Files.exists(newFilePath)) {
-							Files.copy(entry.getValue(), ve.getPathToFile().getParent().resolve(entry.getValue().getFileName()));
-							return true;
-						}
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+					Path newFilePath = ve.getPathToFile().getParent()
+							.resolve(ve.getFileName() + entry.getKey().substring(entry.getKey().lastIndexOf(".")));
+					if (!Files.exists(newFilePath)) {
+						Files.copy(entry.getValue(), newFilePath);
+						return true;
 					}
+				} catch (IOException e1) {
+					System.out.println("Bad Value:" + entry.getValue());
+					e1.printStackTrace();
 				}
+			}
 		}
-		if(ve.getAcceptableFileName().equals(entry.getKey())) {
+		if (ve.getAcceptableFileName().equals(entry.getKey())) {
 			try {
 				Path newFilePath = ve.getPathToFile().getParent().resolve(entry.getValue().getFileName());
-				if(!Files.exists(newFilePath)) {
-					Files.copy(entry.getValue(), ve.getPathToFile().getParent().resolve(entry.getValue().getFileName()));
+				if (!Files.exists(newFilePath)) {
+					Files.copy(entry.getValue(),
+							ve.getPathToFile().getParent().resolve(entry.getValue().getFileName()));
 					return true;
 				}
 			} catch (IOException e1) {
@@ -142,7 +150,7 @@ public class TimedFileScanner extends SimpleFileVisitor<Path> implements Runnabl
 
 	@Override
 	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-		
+
 		if (acceptFile(file)) {
 			VideoEntry vfb = new VideoEntry(file);
 			if (!acceptedFiles.contains(vfb)) {
